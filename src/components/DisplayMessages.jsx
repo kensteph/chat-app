575390ef40a2d53643b3a7ed9d1206b9dd6e0c3e
Kender ROMAIN <kenderromain@gmail.com>
2023-03-01 19:14:14 -0400

    Create DisplayMessages component.

---
+++ src/components/DisplayMessages.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DisplayMessages = () => (
+  <div className="MessageListContainer">DisplayMessages</div>
+);
+
+export default DisplayMessages;
--- src/components/RightSection.jsx
+++ src/components/RightSection.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import DisplayMessages from './DisplayMessages';
+import InputMessage from './InputMessage';
+import RightHeader from './RightHeader';
 
 const RightSection = () => (
-  <div className="rightSection">RightSection</div>
+  <div className="rightSection">
+    <RightHeader />
+    <DisplayMessages />
+    <InputMessage />
+  </div>
 );
 
 export default RightSection;
